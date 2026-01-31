ac69a403ed78d2e60d2fd918d641c4bc7a8f7320
ellyyy-son <ellysonolegario@gmail.com>
2026-02-01 00:56:59 +0800

    Update Form.tsx

--- components/Form.tsx
+++ components/Form.tsx
@@ -4,25 +4,30 @@ import { useState } from 'react';
 export default function ContactForm() {
   const [result, setResult] = useState("");
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    setResult("Sending....");
-    const formData = new FormData(event.target);
-    formData.append("access_key", "bf970cd1-b9c6-4812-a6e0-dc43c35cfe84");
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+  setResult("Sending....");
 
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData
-    });
+  const form = event.currentTarget;
+  const formData = new FormData(form);
 
-    const data = await response.json();
-    if (data.success) {
-      setResult("Form Submitted Successfully");
-      event.target.reset();
-    } else {
-      setResult("Error");
-    }
+  formData.append("access_key", "bf970cd1-b9c6-4812-a6e0-dc43c35cfe84");
+
+  const response = await fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    body: formData,
+  });
+
+  const data = await response.json();
+
+  if (data.success) {
+    setResult("Form Submitted Successfully");
+    form.reset();
+  } else {
+    setResult("Error");
+  }
   };
+
 
   return (
     <form onSubmit={onSubmit} className="w-full lg:w-[70%] mx-auto flex flex-col gap-6 rounded-2xl bg-white/10 p-8 text-white shadow-lg backdrop-blur">
